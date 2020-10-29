83c206d8e2af0f553c9aefe8beb394fc09d58d1f
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-10-29 17:45:01 -0400

    database connected to DB

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.eventNames.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow
+      .bold
+  )
+);
